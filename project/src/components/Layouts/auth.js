156db5fe4ab432723cbe87f7da501c761282b385
amariche <insigned12@gmail.com>
2022-11-11 13:16:40 +0100

    feat Login screen

--- project/src/components/Layouts/auth.js
+++ project/src/components/Layouts/auth.js
@@ -1,8 +1,18 @@
 import { ImageBackground, StatusBar, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './styles';
-
+import { COLORS, SIZES } from '../../theme';
+import Bacimage from '../../Assets/img/bg_login.png';
+import FormLogin from '../../pages/Pages1Auth/Login/components/Form';
 const AuthLayout = () => {
+const [error, seterror] = useState(false)
+const [type, settype] = useState(false)
+
+
+// const CustWILog = !Tablet.IsTab ? "90%" : width >= 1200 ? "30%" : "60%";
+
+const CustWILog = true ? "90%" : SIZES.width >= 1200 ? "30%" : "60%";
+
   return (
     <View style={styles.container}>
     <StatusBar backgroundColor={COLORS.primary} barStyle="light-content" />
@@ -10,7 +20,7 @@ const AuthLayout = () => {
     <ImageBackground
       source={Bacimage}
       resizeMode="cover"
-      style={[styles.image, { height: height }]}
+      style={[styles.image, { height: SIZES.height }]}
     >
       {error && <ToastMessages type={type} error={error} />}
       <View
